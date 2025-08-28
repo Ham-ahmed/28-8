a1e2d5089dca918ee2775127854d0732033bfd57
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2025-08-28 20:44:17 +0300

    Add files via upload

---
+++ auto-dcw-key-add_v1.0.8.sh
@@ -0,0 +1,25 @@
+
+#!/bin/sh
+#
+
+wget -O /var/volatile/tmp/enigma2-plugin-extensions-auto-dcw-key-add_v1.0.8_all.ipk "https://raw.githubusercontent.com/Ham-ahmed/28-8/refs/heads/main/enigma2-plugin-extensions-auto-dcw-key-add_v1.0.8_all.ipk"
+wait
+opkg install --force-overwrite /tmp/*.ipk
+wait
+rm -f /var/volatile/tmp/enigma2-plugin-extensions-auto-dcw-key-add_v1.0.8_all.ipk
+wait
+sleep 2;
+echo "" 
+echo "" 
+echo "#########################################################"
+echo "#                INSTALLED SUCCESSFULLY                 #"
+echo "#                    ON - Panel v6                      #"
+echo "#             Enigma2 restart is required               #"
+echo "#        .::UPLOADED BY  >>>>   HAMDY_AHMED::.          #"
+echo "#     https://www.facebook.com/share/g/18qCRuHz26/      #"
+echo "#########################################################"
+echo "#           your Device will RESTART Now                #"
+echo "#########################################################"
+wait
+killall -9 enigma2
+exit 0
